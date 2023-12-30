ce25918cc263f2d71e469e1090178cfde95e1c4c
venTechie <153870525+venTechie@users.noreply.github.com>
2023-12-30 16:01:38 +0530

    Create goud.ts
    goud.ts

---
+++ src/app/model/goud.ts
@@ -0,0 +1,11 @@
+export interface goud {
+    id: string;
+    Name: string;
+    phone_Number: string;
+    district: string;
+    mandal: string;
+    Village: string;
+    created_Date: string;
+    crated_Time: string;
+    created_By: string;
+}
