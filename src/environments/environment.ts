ce6e36b01ec1b84bcf8aea9a214727054cc7ceef
venTechie <153870525+venTechie@users.noreply.github.com>
2023-12-30 15:34:37 +0530

    Create environment.ts
    environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+    production: false,
+
+    firebaseConfig : {
+        apiKey: "AIzaSyAWyLNs35T0EoI0_XHSdE_Y2BHTZ46ME2A",
+        authDomain: "goudanna-7513a.firebaseapp.com",
+        projectId: "goudanna-7513a",
+        storageBucket: "goudanna-7513a.appspot.com",
+        messagingSenderId: "412979076796",
+        appId: "1:412979076796:web:58856f9efb228ee278bab9"
+      }
+
+};
