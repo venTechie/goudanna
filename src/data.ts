7b84e67791a0fb604a03e7d25829386b5e4a533f
venTechie <153870525+venTechie@users.noreply.github.com>
2023-12-30 15:12:27 +0530

    Create data.ts
    added sample ts file

---
+++ src/data.ts
@@ -0,0 +1,53 @@
+export const districts = [
+    {id:1, name:"ADILABAD"},
+    {id:2, name:"BHADRADRI KOTHAGUDEM"},
+    {id:3, name:"HANUMAKONDA"},
+    {id:4, name:"HYDERABAD"},
+    {id:5, name:"JAGTIAL"},
+    {id:6, name:"JANGOAN"},
+    {id:7, name:"JAYASHANKAR BHOOPALPALLY"},
+    {id:8, name:"JOGULAMBA GADWAL KOTHAGUDEM"},
+    {id:9, name:"KAMAREDDY"},
+    {id:10, name:"KARIMNAGAR"},
+    {id:11, name:"KHAMMAM"},
+    {id:12, name:"KOMARAM BHEEM ASIFABAD"},
+    {id:13, name:"MAHABUBABAD"},
+    {id:14, name:"MAHABUBNAGAR"},
+    {id:15, name:"MANCHERIAL"},
+    {id:16, name:"MEDAK"},
+    {id:17, name:"MEDCHAL-MALKAJGIRI"},
+    {id:18, name:"MULUG"},
+    {id:19, name:"NAGARKURNOOL"},
+    {id:20, name:"NALGONDA"},
+    {id:21, name:"NARAYANPET"},
+    {id:22, name:"NIRMAL"},
+    {id:23, name:"NIZAMABAD"},
+    {id:24, name:"PEDDAPALLI"},
+    {id:25, name:"RAJANNA SIRCILLA"},
+    {id:26, name:"RANGAREDDY"},
+    {id:27, name:"SANGAREDDY"},
+    {id:28, name:"SIDDIPET"},
+    {id:29, name:"SURYAPET"},
+    {id:30, name:"VIKARABAD"},
+    {id:31, name:"WANAPARTHY"},
+    {id:32, name:"WARANGAL"},
+    {id:33, name:"YADADRI BHUVANAGIRI"},
+]
+
+export const mandals = [
+    {id:1, districtid:1, name: "Name1"},
+    {id:2, districtid:1, name: "Name2"},
+    {id:3, districtid:2, name: "Name3"},
+    {id:4, districtid:2, name: "Name4"}
+]
+
+export const villages = [
+    {id:1, mandalid:1, name: "Name1"},
+    {id:2, mandalid:1, name: "Name2"},
+    {id:3, mandalid:2, name: "Name3"},
+    {id:4, mandalid:2, name: "Name4"},
+    {id:5, mandalid:3, name: "Name5"},
+    {id:6, mandalid:3, name: "Name6"},
+    {id:7, mandalid:4, name: "Name7"},
+    {id:8, mandalid:4, name: "Name8"}
+]
